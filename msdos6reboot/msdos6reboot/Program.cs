46076959e4de8800ee3bf50f996d95f0043be9da
TheDragonHawk <jacobgriffith3@gmail.com>
2017-03-15 15:55:39 -0500

    Added shutdown and dir

--- msdos6reboot/msdos6reboot/Program.cs
+++ msdos6reboot/msdos6reboot/Program.cs
@@ -8,6 +8,8 @@ namespace FirstApp
 {
     class Program
     {
+        private static int exitCode;
+
         static void Main(string[] args)
         {
             // Initial Boot
@@ -26,7 +28,7 @@ namespace FirstApp
                 if (input.Equals("chkdsk"))
                 {
                     Console.WriteLine();
-                    Console.WriteLine("Volume MS-DOS_6     created 03-09-1982 8:00a");
+                    Console.WriteLine("Volume MS-DOS_6     created 03-09-1991 8:00a");
                     Console.WriteLine("Volume Serial Number is 4A69-401B");
                     Console.WriteLine();
                     Console.WriteLine("1,073,168,384 bytes total disk space");
@@ -45,6 +47,30 @@ namespace FirstApp
                     Console.WriteLine("Instead of using chkdsk, try using scandisk. scandisk can reliably detect");
                     Console.WriteLine("and fix a much wider range of disk problems. For more information,");
                     Console.WriteLine("type help scandisk from the command prompt.");
+                    Console.WriteLine();
+                }
+                if (input.Equals("shutdown"))
+                {
+                    Console.WriteLine(); 
+                    Console.WriteLine("Stopping services");
+                    Console.WriteLine(@"Ending C:\DOS\SMARTDRV.EXE");
+                    Console.WriteLine("Your system will now shut down");
+                    Environment.Exit(exitCode);
+                }
+                if (input.Equals("dir"))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(" Volume in drive C is MS-DOS_6");
+                    Console.WriteLine(" Volume Serial Number is 4A69-401B");
+                    Console.WriteLine(@" Directory of C:\");
+                    Console.WriteLine("DOS           <DIR>         03-09-91   8:00a");
+                    Console.WriteLine("COMMAND  COM         54,645 01-02-91   6:22a");
+                    Console.WriteLine("WINA20   386          9,349 01-02-91   6:22a");
+                    Console.WriteLine("CONFIG   SYS             71 03-09-91   8:02a");
+                    Console.WriteLine("AUTOEXEC BAT             78 03-09-91   8:02a");
+                    Console.WriteLine("        5 file(s)          64,153 bytes");
+                    Console.WriteLine("                   1,066,074,112 bytes free");
+                    Console.WriteLine();
                 }
             }
         }
